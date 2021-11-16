5e908b418283980d27ab002f1ac6541c0762adf1
zchwood <zchwood258@gmail.com>
2021-11-15 23:17:04 -0500

    added multiple instances allowed to views on theOffice

--- users/views.py
+++ users/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers import theOfficeSerializer, GOTSerializer, random_userSerializer
 from .models import theOffice, GOT, random_user
+from rest_framework.response import Response
+
 
 # Create your views here.
 class theOfficeViewSet(viewsets.ModelViewSet):
     queryset = theOffice.objects.all().order_by('name')
     serializer_class = theOfficeSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class GOTViewSet(viewsets.ModelViewSet):
     queryset = GOT.objects.all().order_by('name')
