c6da3b65a1aaa770852bf3cdcb3bed9c731fc1e6
zchwood <zchwood258@gmail.com>
2021-11-15 23:25:00 -0500

    fixed multiple posts on all models

--- users/views.py
+++ users/views.py
@@ -22,6 +22,20 @@ class GOTViewSet(viewsets.ModelViewSet):
     queryset = GOT.objects.all().order_by('name')
     serializer_class = GOTSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 class random_userViewSet(viewsets.ModelViewSet):
     queryset = random_user.objects.all().order_by('name')
     serializer_class = random_userSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data,list))
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
